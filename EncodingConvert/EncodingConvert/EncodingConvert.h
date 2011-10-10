79c266204e6adc59d9dd25dfa0e1e66c146a3a84
feiatc <qhkyzf@gmail.com>
2011-10-10 16:46:55 +0800

    add some code but error

--- EncodingConvert/EncodingConvert/EncodingConvert.h
+++ EncodingConvert/EncodingConvert/EncodingConvert.h
@@ -22,4 +22,36 @@
 + (NSString *)convertUTF8ToUnicode:(NSString *)aUTF8;
 + (NSString *)convertGBKToUnicode:(NSString *)aGBK;
 
+// Calculate MD5
++ (NSString *MD5(NSString *str);
+
+// Calculate HMAC SHA1
++ (NSString *HmacSHA1(NSString *text, NSString *secret);
+
+// BASE64 encode
++ (NSString *BASE64Encode(const unsigned char *data, NSUInteger length, NSUInteger lineLength = 0);
+
+// BASE64 decode
++ NSData *BASE64Decode(NSString *string);
+
+// BASE64 encode data
++ NS_INLINE NSString *BASE64EncodeData(NSData *data, NSUInteger lineLength = 0)
+{
+	return BASE64Encode((const unsigned char *)data.bytes, data.length, lineLength);
+}
+
+// BASE64 encode string
++ NS_INLINE NSString *BASE64EncodeString(NSString *string, NSUInteger lineLength = 0)
+{
+	return BASE64EncodeData([string dataUsingEncoding:NSUTF8StringEncoding], lineLength);
+}
+
+// BASE64 decode string
++ NS_INLINE NSString *BASE64DecodeString(NSString *string)
+{
+	NSData *data = BASE64Decode(string);
+	return [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+}
+
+
 @end
--- EncodingConvert/EncodingConvert/EncodingConvert.mm
+++ EncodingConvert/EncodingConvert/EncodingConvert.mm
@@ -222,4 +222,188 @@
 
 }
 
+// Calculate MD5
+NSString *NSUtil::MD5(NSString *str)
+{
+	if (str == nil) return nil;
+	unsigned char result[CC_MD5_DIGEST_LENGTH];
+	const char *cstr = [str UTF8String];
+	CC_MD5(cstr, strlen(cstr), result);
+	return [NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+			result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
+			result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]];
+}
+
+// Calculate SHA1
+NSString *NSUtil::HmacSHA1(NSString *text, NSString *secret)
+{
+	NSData *secretData = [secret dataUsingEncoding:NSUTF8StringEncoding];
+	NSData *clearTextData = [text dataUsingEncoding:NSUTF8StringEncoding];
+	
+	unsigned char result[20];
+	CCHmac(kCCHmacAlgSHA1, [secretData bytes], [secretData length], [clearTextData bytes], [clearTextData length], result);
+	return BASE64Encode(result, 20);
+}
+
+// BASE64 encode
+NSString *NSUtil::BASE64Encode(const unsigned char *data, NSUInteger length, NSUInteger lineLength)
+{
+	// BASE64 table
+	const static char c_baseTable[64] =
+	{
+		'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+		'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
+		'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+		'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'
+	};
+	
+	NSMutableString *result = [NSMutableString stringWithCapacity:length];
+	unsigned long ixtext = 0;
+	unsigned long lentext = length;
+	long ctremaining = 0;
+	unsigned char inbuf[3], outbuf[4];
+	short i = 0;
+	short charsonline = 0, ctcopy = 0;
+	unsigned long ix = 0;
+	
+	while (YES)
+	{
+		ctremaining = lentext - ixtext;
+		if (ctremaining <= 0) break;
+		
+		for (i = 0; i < 3; i++)
+		{
+			ix = ixtext + i;
+			if (ix < lentext)
+			{
+				inbuf[i] = data[ix];
+			}
+			else
+			{
+				inbuf [i] = 0;
+			}
+		}
+		
+		outbuf [0] = (inbuf [0] & 0xFC) >> 2;
+		outbuf [1] = ((inbuf [0] & 0x03) << 4) | ((inbuf [1] & 0xF0) >> 4);
+		outbuf [2] = ((inbuf [1] & 0x0F) << 2) | ((inbuf [2] & 0xC0) >> 6);
+		outbuf [3] = inbuf [2] & 0x3F;
+		ctcopy = 4;
+		
+		switch (ctremaining)
+		{
+			case 1: 
+				ctcopy = 2; 
+				break;
+			case 2: 
+				ctcopy = 3; 
+				break;
+		}
+		
+		for (i = 0; i < ctcopy; i++)
+		{
+			[result appendFormat:@"%c", c_baseTable[outbuf[i]]];
+		}
+		
+		for (i = ctcopy; i < 4; i++)
+		{
+			[result appendFormat:@"%c",'='];
+		}
+		
+		ixtext += 3;
+		charsonline += 4;
+		
+		if (lineLength > 0)
+		{
+			if (charsonline >= lineLength)
+			{
+				charsonline = 0;
+				[result appendString:@"\n"];
+			}
+		}
+	}
+	
+	return result;
+}
+
+// BASE64 decode
+NSData *NSUtil::BASE64Decode(NSString *string)
+{
+	NSMutableData *mutableData = nil;
+	
+	if (string)
+	{
+		unsigned long ixtext = 0;
+		unsigned long lentext = 0;
+		unsigned char ch = 0;
+		unsigned char inbuf[3], outbuf[4];
+		short i = 0, ixinbuf = 0;
+		BOOL flignore = NO;
+		BOOL flendtext = NO;
+		NSData *base64Data = nil;
+		const unsigned char *base64data = nil;
+		
+		// Convert the string to ASCII data.
+		base64Data = [string dataUsingEncoding:NSASCIIStringEncoding];
+		base64data = (const unsigned char *)[base64Data bytes];
+		mutableData = [NSMutableData dataWithCapacity:[base64Data length]];
+		lentext = [base64Data length];
+		
+		while (YES)
+		{
+			if( ixtext >= lentext )
+			{
+				break;
+			}
+			ch = base64data[ixtext++];
+			flignore = NO;
+			
+			if ((ch >= 'A') && (ch <= 'Z')) ch = ch - 'A';
+			else if ((ch >= 'a') && (ch <= 'z')) ch = ch - 'a' + 26;
+			else if ((ch >= '0') && (ch <= '9')) ch = ch - '0' + 52;
+			else if (ch == '+') ch = 62;
+			else if (ch == '=') flendtext = YES;
+			else if (ch == '/') ch = 63;
+			else flignore = YES; 
+			
+			if (!flignore)
+			{
+				short ctcharsinbuf = 3;
+				BOOL flbreak = NO;
+				
+				if (flendtext)
+				{
+					if (!ixinbuf) break;
+					if ((ixinbuf == 1) || (ixinbuf == 2)) ctcharsinbuf = 1;
+					else ctcharsinbuf = 2;
+					ixinbuf = 3;
+					flbreak = YES;
+				}
+				
+				inbuf [ixinbuf++] = ch;
+				
+				if (ixinbuf == 4)
+				{
+					ixinbuf = 0;
+					outbuf [0] = (inbuf[0] << 2) | ((inbuf[1] & 0x30) >> 4);
+					outbuf [1] = ((inbuf[1] & 0x0F) << 4) | ((inbuf[2] & 0x3C) >> 2);
+					outbuf [2] = ((inbuf[2] & 0x03) << 6) | (inbuf[3] & 0x3F);
+					
+					for (i = 0; i < ctcharsinbuf; i++)
+					{
+						[mutableData appendBytes:&outbuf[i] length:1];
+					}
+				}
+				
+				if (flbreak)
+				{
+					break;
+				}
+			}
+		}
+	}
+	
+	return mutableData;
+}
+
 @end
