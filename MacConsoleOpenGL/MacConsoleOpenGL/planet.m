af5a413445a0af452126cfa8a0f9cca6e5e55f81
feiqhk <flying86@foxmail.com>
2011-11-09 23:27:20 +0800

    planet translate rotate

--- MacConsoleOpenGL/MacConsoleOpenGL/planet.m
+++ MacConsoleOpenGL/MacConsoleOpenGL/planet.m
@@ -10,25 +10,88 @@
 #import <OpenGL/OpenGL.h>
 #import <GLUT/GLUT.h>
 
+static int year = 0, day = 0, day2 = 0;
+
 void initPlanet()
 {
-	
+	glClearColor(0, 0, 0, 0);
+	glShadeModel(GL_FLAT);
 }
 
 
 void displayPlanet()
 {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1, 1, 1);
+	glPushMatrix();
+	glutWireSphere(1.0, 20, 16);
 	
+	glRotatef((GLfloat)year, 0, 1, 0);
+	glTranslatef(2, 0, 0);
+	
+	glPushMatrix();
+	glRotatef((GLfloat)day, 0, 1, 0);
+	glutWireSphere(0.6, 16, 10);
+	glPopMatrix();
+	
+	glRotatef((GLfloat)day2, 1, 0, 0);
+	glTranslatef(0, 1.2, 0);
+	glutWireSphere(0.3, 10, 8);
+	
+
+	glPopMatrix();
+	
+	glutSwapBuffers();
 }
 
 void reshapePlanet(int w, int h)
 {
-	
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60, w/h, 1, 20);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
 }
 
 void keyboardPlanet(unsigned char key, int x, int y)
 {
-	
+	switch (key)
+	{
+		case 'd':
+		{
+			day = (day + 10) % 360;
+			day2 = (day2 + 5) % 360;
+			glutPostRedisplay();
+		}
+		break;
+			
+		case 'D':
+		{
+			day = (day - 10) % 360;
+			day2 = (day2 - 5) % 360;
+			glutPostRedisplay();	
+		}
+		break;
+			
+		case 'y':
+		{
+			year = (year + 5) % 360;
+			glutPostRedisplay();
+		}
+		break;
+			
+		case 'Y':
+		{
+			year = (year - 5) % 360;
+			glutPostRedisplay();	
+		}
+		break;
+			
+		default:
+			break;
+	}
 }
 
 int mainPlanet(int argc, const char * argv[])
