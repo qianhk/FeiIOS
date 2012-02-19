2b641f0230e29361d088a20583af4f4e06f0fe10
feiqhk <flying86@foxmail.com>
2012-02-19 22:47:51 +0800

    add kai's iconrenamer from rpetrich for exercise.

---
+++ iconrenamer/iconrenamer/iconrenamer.m
@@ -0,0 +1,231 @@
+//#import <uikit/uikit.h>
+#import <SpringBoard/SpringBoard.h>
+#import <CaptainHook/CaptainHook.h>
+
+#import "SBIconView.h"
+
+@interface SBIcon (OS40)
+- (NSString *)leafIdentifier;
+- (void)updateLabel;
+@end
+
+static NSMutableDictionary *iconMappings;
+
+#define kSettingsFilePath "/var/mobile/Library/Preferences/com.njnu.kai.iconrenamer.plist"
+
+__attribute__((visibility("hidden")))
+@interface IconRenamer : NSObject <UIAlertViewDelegate, UITextFieldDelegate> {
+@private
+	SBIcon *_icon;
+	SBIconView *_iconView;
+	UIAlertView *_av;
+	BOOL _hasTouch;
+}
+- (id)initWithIcon:(SBIcon *)icon iconView:(SBIconView *)iconView;
+- (void)receiveTouch;
+@end
+
+static NSInteger originalName;
+
+@implementation IconRenamer
+
+static IconRenamer *currentRenamer;
+
++ (id)renamerWithIcon:(SBIcon *)icon iconView:(SBIconView *)iconView
+{
+	return [[[self alloc] initWithIcon:icon iconView:iconView] autorelease];
+}
+
+- (id)initWithIcon:(SBIcon *)icon iconView:(SBIconView *)iconView
+{
+	if ((self = [super init])) {
+		currentRenamer = self;
+		_icon = [icon retain];
+		_iconView = [iconView retain];
+	}
+	return self;
+}
+
+- (void)receiveTouch
+{
+	if (!_hasTouch) {
+		_hasTouch = YES;
+		[self performSelector:@selector(show) withObject:nil afterDelay:0.25];
+	} else {
+		[NSObject cancelPreviousPerformRequestsWithTarget:self];
+	}
+}
+
+- (void)show
+{
+	if (!_av) {
+		_av = [[UIAlertView alloc] init];
+		_av.delegate = self;
+		originalName++;
+		NSString *title = [_icon displayName];
+		originalName--;
+		_av.title = [@"Rename " stringByAppendingString:title];
+		UITextField *textField = [_av addTextFieldWithValue:[_icon displayName] label:nil];
+		textField.delegate = self;
+		textField.returnKeyType = UIReturnKeyDone;
+		textField.clearButtonMode = UITextFieldViewModeAlways;
+		_av.cancelButtonIndex = [_av addButtonWithTitle:@"Cancel"];
+		[_av addButtonWithTitle:@"Apply"];
+		[_av show];
+		[self retain];
+	}
+}
+
+- (void)save
+{
+	NSString *identifier = [_icon leafIdentifier];
+	NSString *newDisplayName = [[_av textFieldAtIndex:0] text];
+	if (![[_icon displayName] isEqualToString:newDisplayName]) {
+		[iconMappings setObject:newDisplayName forKey:identifier];
+		[iconMappings writeToFile:@kSettingsFilePath atomically:YES];
+		if (_iconView)
+			[_iconView updateLabel];
+		else
+			[_icon updateLabel];
+	}
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+	textField.delegate = nil;
+	[self save];
+	_av.delegate = nil;
+	[_av dismissWithClickedButtonIndex:0 animated:YES];
+	[_av release];
+	_av = nil;
+	[self release];
+	return NO;
+}
+
+- (void)alertView:(UIAlertView *)av clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+	if (buttonIndex != _av.cancelButtonIndex)
+		[self save];
+	[[_av textFieldAtIndex:0] setDelegate:nil];
+	_av.delegate = nil;
+	[_av release];
+	_av = nil;
+	[self release];
+}
+
+- (void)dealloc
+{
+	if (currentRenamer == self)
+		currentRenamer = nil;
+	[_icon release];
+	[_iconView release];
+	[super dealloc];
+}
+
+@end
+
+CHDeclareClass(SBApplicationIcon);
+CHDeclareClass(SBIconController);
+
+CHOptimizedMethod(0, self, NSString *, SBApplicationIcon, displayName)
+{
+	if (originalName == 0) {
+		NSString *title = [iconMappings objectForKey:[self leafIdentifier]];
+		if (title)
+			return title;
+	}
+	return CHSuper(0, SBApplicationIcon, displayName);
+}
+
+static BOOL inTap;
+static NSTimeInterval lastTapTime;
+static SBApplicationIcon *lastTapIcon;
+
+CHOptimizedMethod(2, super, void, SBApplicationIcon, touchesBegan, NSSet *, touches, withEvent, UIEvent *, event)
+{
+	inTap = [CHSharedInstance(SBIconController) isEditing];
+	CHSuper(2, SBApplicationIcon, touchesBegan, touches, withEvent, event);
+}
+
+CHOptimizedMethod(2, super, void, SBApplicationIcon, touchesMoved, NSSet *, touches, withEvent, UIEvent *, event)
+{
+	inTap = NO;
+	CHSuper(2, SBApplicationIcon, touchesMoved, touches, withEvent, event);
+}
+
+CHOptimizedMethod(2, super, void, SBApplicationIcon, touchesEnded, NSSet *, touches, withEvent, UIEvent *, event)
+{
+	if (inTap) {
+		if ([[iconMappings objectForKey:@"IRRequiresDoubleTap"] boolValue]) {
+			UITouch *touch = [touches anyObject];
+			NSTimeInterval currentTapTime = touch.timestamp;
+			if ((currentTapTime - lastTapTime < 0.5) && (lastTapIcon == self))
+				[[IconRenamer renamerWithIcon:self iconView:nil] show];
+			[lastTapIcon autorelease];
+			lastTapIcon = [self retain];
+			lastTapTime = currentTapTime;
+		} else {
+			[currentRenamer ?: [IconRenamer renamerWithIcon:self iconView:nil] receiveTouch];
+		}
+	}
+	CHSuper(2, SBApplicationIcon, touchesEnded, touches, withEvent, event);
+}
+
+CHDeclareClass(SBIconView)
+
+static SBIconView *lastTapIconView;
+
+CHOptimizedMethod(2, super, void, SBIconView, touchesBegan, NSSet *, touches, withEvent, UIEvent *, event)
+{
+	inTap = [CHSharedInstance(SBIconController) isEditing];
+	CHSuper(2, SBIconView, touchesBegan, touches, withEvent, event);
+}
+
+CHOptimizedMethod(2, super, void, SBIconView, touchesMoved, NSSet *, touches, withEvent, UIEvent *, event)
+{
+	inTap = NO;
+	CHSuper(2, SBIconView, touchesMoved, touches, withEvent, event);
+}
+
+CHOptimizedMethod(2, super, void, SBIconView, touchesEnded, NSSet *, touches, withEvent, UIEvent *, event)
+{
+	if (inTap) {
+		SBIcon *icon = self.icon;
+		if ([icon isKindOfClass:CHClass(SBApplicationIcon)]) {
+			if ([[iconMappings objectForKey:@"IRRequiresDoubleTap"] boolValue]) {
+				UITouch *touch = [touches anyObject];
+				NSTimeInterval currentTapTime = touch.timestamp;
+				if ((currentTapTime - lastTapTime < 0.5) && (lastTapIconView == self))
+					[[IconRenamer renamerWithIcon:self.icon iconView:self] show];
+				[lastTapIconView autorelease];
+				lastTapIconView = [self retain];
+				lastTapTime = currentTapTime;
+			} else {
+				[currentRenamer ?: [IconRenamer renamerWithIcon:self.icon iconView:self] receiveTouch];
+			}
+		}
+	}
+	CHSuper(2, SBIconView, touchesEnded, touches, withEvent, event);
+}
+
+static void LoadSettings()
+{
+	[iconMappings release];
+	iconMappings = [[NSMutableDictionary alloc] initWithContentsOfFile:@kSettingsFilePath] ?: [[NSMutableDictionary alloc] init];
+}
+
+CHConstructor {
+	CHLoadLateClass(SBApplicationIcon);
+	CHHook(0, SBApplicationIcon, displayName);
+	CHHook(2, SBApplicationIcon, touchesBegan, withEvent);
+	CHHook(2, SBApplicationIcon, touchesMoved, withEvent);
+	CHHook(2, SBApplicationIcon, touchesEnded, withEvent);
+	CHLoadLateClass(SBIconView);
+	CHHook(2, SBIconView, touchesBegan, withEvent);
+	CHHook(2, SBIconView, touchesMoved, withEvent);
+	CHHook(2, SBIconView, touchesEnded, withEvent);
+	CHLoadLateClass(SBIconController);
+	CHAutoreleasePoolForScope();
+	CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (void *)LoadSettings, CFSTR("ch.rpetri.iconrenamer/settingschanged"), NULL, CFNotificationSuspensionBehaviorCoalesce);
+	LoadSettings();
+}
---
+++ iconrenamer/iconrenamer/iconrenamer2.mm
@@ -0,0 +1,124 @@
+
+//
+//  Created by kai on 12-2-19.
+//  Copyright (c) 2012年 njnu. All rights reserved.
+//
+
+// CaptainHook by Ryan Petrich
+// see https://github.com/rpetrich/CaptainHook/
+
+//#import <Foundation/Foundation.h>
+//#import <uikit/uikit.h>
+#import <CaptainHook/CaptainHook.h>
+#include <notify.h> // not required; for examples only
+#import <springboard/springboard.h>
+
+
+
+
+
+//__attribute__((constructor))
+//static void initialize() {
+//	NSLog(@"MyExt: Loaded");
+////	MSHookFunction(CFShow, replaced_CFShow, &original_CFShow);
+//}
+
+/*
+%hook SpringBoard
+
+-(void)applicationDidFinishLaunching:(id)application {
+    %orig;
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Welcome" 
+													message:@"Welcome to your iPhone Brandon!" 
+												   delegate:nil 
+										  cancelButtonTitle:@"Thanks" 
+										  otherButtonTitles:nil];
+    [alert show];
+    [alert release];
+}
+
+%end
+*/
+
+// Objective-C runtime hooking using CaptainHook:
+//   1. declare class using CHDeclareClass()
+//   2. load class using CHLoadClass() or CHLoadLateClass() in CHConstructor
+//   3. hook method using CHOptimizedMethod()
+//   4. register hook using CHHook() in CHConstructor
+//   5. (optionally) call old method using CHSuper()
+
+
+/*
+@interface kaisubstrate3 : NSObject
+
+@end
+
+@implementation kaisubstrate3
+
+-(id)init
+{
+	if ((self = [super init]))
+	{
+		NSInteger a = 0;
+		++a;
+		CGRect rect = CGRectMake(0, 0, 100, 50);
+		rect.origin.x = 10;
+	}
+
+    return self;
+}
+
+@end
+
+
+@class ClassToHook;
+
+CHDeclareClass(ClassToHook); // declare class
+
+CHOptimizedMethod(0, self, void, ClassToHook, messageName) // hook method (with no arguments and no return value)
+{
+	// write code here ...
+	
+	CHSuper(0, ClassToHook, messageName); // call old (original) method
+}
+
+CHOptimizedMethod(2, self, BOOL, ClassToHook, arg1, NSString*, value1, arg2, BOOL, value2) // hook method (with 2 arguments and a return value)
+{
+	// write code here ...
+
+	return CHSuper(2, ClassToHook, arg1, value1, arg2, value2); // call old (original) method and return its return value
+}
+
+static void WillEnterForeground(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+{
+	// not required; for example only
+}
+
+static void ExternallyPostedNotification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+{
+	// not required; for example only
+}
+
+CHConstructor // code block that runs immediately upon load
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	// listen for local notification (not required; for example only)
+	CFNotificationCenterRef center = CFNotificationCenterGetLocalCenter();
+	CFNotificationCenterAddObserver(center, NULL, WillEnterForeground, CFSTR("UIApplicationWillEnterForegroundNotification"), NULL, CFNotificationSuspensionBehaviorCoalesce);
+	
+	// listen for system-side notification (not required; for example only)
+	// this would be posted using: notify_post("cn.njnu.kaisubstrate3.eventname");
+	CFNotificationCenterRef darwin = CFNotificationCenterGetDarwinNotifyCenter();
+	CFNotificationCenterAddObserver(darwin, NULL, ExternallyPostedNotification, CFSTR("cn.njnu.kaisubstrate3.eventname"), NULL, CFNotificationSuspensionBehaviorCoalesce);
+	
+	// CHLoadClass(ClassToHook); // load class (that is "available now")
+	// CHLoadLateClass(ClassToHook);  // load class (that will be "available later")
+	
+	CHHook(0, ClassToHook, messageName); // register hook
+	CHHook(2, ClassToHook, arg1, arg2); // register hook
+	
+	[pool drain];
+}
+*/
