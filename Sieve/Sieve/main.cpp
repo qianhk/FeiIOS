57d6710165532f51881f3fe9d7fc86666f9c9cf2
feiatc <qhkyzf@gmail.com>
2012-04-23 17:15:15 +0800

    add project sieve, compare with java

---
+++ Sieve/Sieve/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sieve
+//
+//  Created by hongkai.qian on 12-4-23.
+//  Copyright (c) 2012年 TTPod. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
