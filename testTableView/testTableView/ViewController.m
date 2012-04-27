4765285e26df769ac4d8e736650c9684e8745b59
feiqhk <flying86@foxmail.com>
2012-04-28 00:19:20 +0800

    change height

--- testTableView/testTableView/ViewController.m
+++ testTableView/testTableView/ViewController.m
@@ -33,7 +33,7 @@
 	_tv = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].bounds style:UITableViewStylePlain];
 	[_tv setDelegate:self];
 	[_tv setDataSource:self];
-	_arrText = [[NSArray alloc] initWithObjects:@"国外媒体今天发表署名为Jessica E. Vascellaro的文章称，对于苹果来说，中国已经不再是无足轻重的市场，而是已经变成了其“中央王国”。", @"凯凯Test", @"就在不久以前，亚太市场在科技巨子苹果的财务报告中还只不过是个脚注。但现在已不再如此。", @"我设置了imageview的 大小，然后把。", nil];
+	_arrText = [[NSArray alloc] initWithObjects:@"国外媒体今天发表署名为Jessica E. Vascellaro的文章称，对于苹果来说，中国已经不再是无足轻重的市场，而是已经变成了其“中央王国”。", @"凯凯Test", @"我设置了mageview的大小，然后把。", @"就在不久以前，亚太市场在科技巨子苹果的财务报告中还只不过是个脚注。但现在已不再如此。", nil];
 	[self.view addSubview:_tv];
 	
 	[_tv reloadData];
@@ -41,7 +41,16 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-	return 70.0f;
+	NSString* singleStr = @"哈哈Test";
+	CGFloat singleHeight = [singleStr sizeWithFont:[UIFont systemFontOfSize:14] constrainedToSize:CGSizeMake(200, 1000) lineBreakMode:UILineBreakModeCharacterWrap].height; 
+	CGFloat needHeight = [[_arrText objectAtIndex:indexPath.row] sizeWithFont:[UIFont systemFontOfSize:14] constrainedToSize:CGSizeMake(243, 1000) lineBreakMode:UILineBreakModeCharacterWrap].height;
+	CGFloat detaHeight = needHeight - singleHeight;
+	CGFloat height = 70.0f;
+	if (detaHeight > 0.2 )
+	{
+		height += detaHeight;
+	}
+	return height;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
