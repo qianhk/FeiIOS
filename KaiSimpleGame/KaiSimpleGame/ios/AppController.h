51d7601e4a2421e6bc41373c3c607e14356c0618
qhkyzf <qhkyzf@gmail.com>
2012-11-01 01:19:55 +0800

    add cocos2d-x project kaiSimpleGame

---
+++ KaiSimpleGame/KaiSimpleGame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  KaiSimpleGameAppController.h
+//  KaiSimpleGame
+//
+//  Created by qianhk on 12-11-1.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ KaiSimpleGame/KaiSimpleGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  KaiSimpleGameAppController.h
+//  KaiSimpleGame
+//
+//  Created by qianhk on 12-11-1.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
