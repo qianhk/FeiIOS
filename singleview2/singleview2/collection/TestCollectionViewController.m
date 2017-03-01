6a9f91acf675c6437166edbec7f9c4d73c3dc121
feiatc <qhk83@qq.com>
2017-03-01 15:56:58 +0800

    test sizeForItemAtIndexPath

--- singleview2/singleview2/collection/TestCollectionViewController.m
+++ singleview2/singleview2/collection/TestCollectionViewController.m
@@ -6,13 +6,17 @@
 #import "TestCollectionViewController.h"
 #import "ContentCell.h"
 
+@interface TestCollectionViewController () <UICollectionViewDelegateFlowLayout>
+
+@end
 
 @implementation TestCollectionViewController {
 
 }
 
 - (instancetype)init {
-    self = [super initWithCollectionViewLayout:[[UICollectionViewFlowLayout alloc] init]];
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    self = [super initWithCollectionViewLayout:flowLayout];
     if (self) {
 
     }
@@ -27,18 +31,18 @@
     self.collectionView.backgroundColor = [UIColor whiteColor];
 
     self.sections = @[
-            @{@"header" : @"First Section", @"content" : @"first content ha ha empty "},
-            @{@"header" : @"second Section", @"content" : @"second content"},
-            @{@"header" : @"Section 3", @"content" : @"content 3"},
-            @{@"header" : @"Section 4", @"content" : @"content 第第第第第第第第第第第第4 try"},
-            @{@"header" : @"Section 5", @"content" : @"content 第6 word"},
-            @{@"header" : @"Section 666666", @"content" : @"content 666666 origin world"},
+            @{@"header": @"First Section", @"content": @"first content ha ha empty "},
+            @{@"header": @"second Section", @"content": @"second content"},
+            @{@"header": @"Section 3", @"content": @"content 3"},
+            @{@"header": @"Section 4", @"content": @"content 第第第第第第第第第第第第4 try"},
+            @{@"header": @"Section 5", @"content": @"content 第6 word"},
+            @{@"header": @"Section 666666", @"content": @"content 666666 origin world"},
     ];
 
     [self.collectionView registerClass:[ContentCell class] forCellWithReuseIdentifier:@"CONTENT"];
 }
 
-- (NSArray *)worldsInSection: (NSInteger) section {
+- (NSArray *)worldsInSection:(NSInteger)section {
     NSString *content = self.sections[section][@"content"];
     NSCharacterSet *space = [NSCharacterSet whitespaceAndNewlineCharacterSet];
     NSArray *words = [content componentsSeparatedByCharactersInSet:space];
@@ -60,6 +64,12 @@
     cell.maxWidth = collectionView.bounds.size.width;
     cell.text = words[indexPath.row];
     return cell;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSArray *words = [self worldsInSection:indexPath.section];
+    CGSize result = [ContentCell sizeForContentSize:words[indexPath.row] forMaxWidth:collectionView.bounds.size.width];
+    return result;
 }
 
 
