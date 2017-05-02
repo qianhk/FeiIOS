8f66ac548cd86fa19681ca3846949e8c8c513a8e
feiatc <qhk83@qq.com>
2017-05-02 20:52:22 +0800

    test CGRectGetXxxXY

--- singleview2/singleview2/UikitTestViewController.m
+++ singleview2/singleview2/UikitTestViewController.m
@@ -23,6 +23,15 @@
     // Do any additional setup after loading the view.
 //    [self addMask];
     [self addMask2];
+
+    CGRect rect = CGRectMake(10, 20, 100, 200);
+    NSLog(@"CGRectGetHeight--%f", CGRectGetHeight(rect));
+    NSLog(@"CGRectGetMaxX--%f", CGRectGetMaxX(rect));
+    NSLog(@"CGRectGetMaxY--%f", CGRectGetMaxY(rect));
+    NSLog(@"CGRectGetMidX--%f", CGRectGetMidX(rect));
+    NSLog(@"CGRectGetMidY--%f", CGRectGetMidY(rect));
+    NSLog(@"CGRectGetMinX--%f", CGRectGetMinX(rect));
+    NSLog(@"CGRectGetMinY--%f", CGRectGetMinY(rect));
 }
 
 - (void)didReceiveMemoryWarning {
@@ -43,37 +52,37 @@
 static int TOP = 64;
 
 - (void)addMask {
-    UIButton * _maskButton = [[UIButton alloc] init];
+    UIButton *_maskButton = [[UIButton alloc] init];
     [_maskButton setFrame:CGRectMake(0, TOP, SCREEN_WIDTH, 81)];
 //    [_maskButton setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.7]];
     [_maskButton setBackgroundColor:[UIColor blueColor]];
     [self.view addSubview:_maskButton];
-    
+
     //create path
 //    UIBezierPath *path = [UIBezierPath bezierPathWithRect:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
     UIBezierPath *path = [UIBezierPath bezierPath];
-    
+
 //    [path appendPath:[UIBezierPath bezierPathWithRect:CGRectMake(0, TOP, SCREEN_WIDTH, 73)]];
 //    [path appendPath:[UIBezierPath bezierPathWithArcCenter:CGPointMake(SCREEN_WIDTH / 2, TOP + 140) radius:100 startAngle:0 endAngle:2*M_PI clockwise:NO]];
-    
+
 //    // MARK: roundRectanglePath
 //    [path appendPath:[[UIBezierPath bezierPathWithRoundedRect:CGRectMake(20, 400, SCREEN_WIDTH - 2 * 20, 100) cornerRadius:15] bezierPathByReversingPath]];
-    
+
     CAShapeLayer *shapeLayer = [CAShapeLayer layer];
-    
+
     shapeLayer.path = path.CGPath;
-    
+
     [_maskButton.layer setMask:shapeLayer];
 }
 
 - (void)addMask2 {
 
-    UIImageView * maskView = [[UIImageView alloc] init];
+    UIImageView *maskView = [[UIImageView alloc] init];
     [maskView setFrame:CGRectMake(0, 0, SCREEN_WIDTH, 81)];
     [maskView setImage:[UIImage imageNamed:@"circle_mask"]];
 //    [self.view addSubview:maskView];
 
-    UIImageView * button = [[UIImageView alloc] init];
+    UIImageView *button = [[UIImageView alloc] init];
     [button setFrame:CGRectMake(0, TOP, SCREEN_WIDTH, 81)];
     //    [button setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.7]];
     button.backgroundColor = [UIColor blueColor];
@@ -84,24 +93,22 @@ static int TOP = 64;
     button.layer.mask = maskView.layer;
 
 
-
-
-    UIButton * button2 = [[UIButton alloc] init];
+    UIButton *button2 = [[UIButton alloc] init];
     [button2 setFrame:CGRectMake(0, TOP + 200, SCREEN_WIDTH, 81)];
     [button2 setBackgroundColor:[UIColor blueColor]];
     [self.view addSubview:button2];
 
-    UIImageView * maskView2 = [[UIImageView alloc] init];
+    UIImageView *maskView2 = [[UIImageView alloc] init];
     [maskView2 setFrame:CGRectMake(0, TOP + 300, SCREEN_WIDTH, 81)];
     [maskView2 setImage:[UIImage imageNamed:@"circle_mask"]];
     [self.view addSubview:maskView2];
 
-    UIButton * button3 = [[UIButton alloc] init];
+    UIButton *button3 = [[UIButton alloc] init];
     [button3 setFrame:CGRectMake(0, TOP + 400, SCREEN_WIDTH, 81)];
     [button3 setBackgroundColor:[UIColor blueColor]];
     [self.view addSubview:button3];
 
-    UIImageView * maskView3 = [[UIImageView alloc] init];
+    UIImageView *maskView3 = [[UIImageView alloc] init];
     [maskView3 setFrame:CGRectMake(0, TOP + 400, SCREEN_WIDTH, 81)];
     [maskView3 setImage:[UIImage imageNamed:@"circle_mask"]];
     [self.view addSubview:maskView3];
