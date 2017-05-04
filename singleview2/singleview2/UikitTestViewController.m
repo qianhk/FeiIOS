4c08d4ef69e054a7ac1b1b102362b753a19fb617
feiatc <qhk83@qq.com>
2017-05-04 11:06:41 +0800

    use pod and import ReactiveObjC

--- singleview2/singleview2/UikitTestViewController.m
+++ singleview2/singleview2/UikitTestViewController.m
@@ -6,6 +6,9 @@
 //  Copyright © 2017年 Njnu. All rights reserved.
 //
 
+#import <ReactiveObjC/RACSignal.h>
+#import <ReactiveObjC/RACSubscriber.h>
+#import <ReactiveObjC/RACDisposable.h>
 #import "UikitTestViewController.h"
 #import "NSObject+Calculator.h"
 #import "CalculatorMaker.h"
@@ -52,6 +55,49 @@
         return result == 10;
     }];
     NSLog(@"functional calculator result=%d equal=%@", c.result, c.isEqual ? @"YES" : @"NO");
+    
+    
+    // RACSignal使用步骤：
+    // 1.创建信号 + (RACSignal *)createSignal:(RACDisposable * (^)(id<RACSubscriber> subscriber))didSubscribe
+    // 2.订阅信号,才会激活信号. - (RACDisposable *)subscribeNext:(void (^)(id x))nextBlock
+    // 3.发送信号 - (void)sendNext:(id)value
+    
+    
+    // RACSignal底层实现：
+    // 1.创建信号，首先把didSubscribe保存到信号中，还不会触发。
+    // 2.当信号被订阅，也就是调用signal的subscribeNext:nextBlock
+    // 2.2 subscribeNext内部会创建订阅者subscriber，并且把nextBlock保存到subscriber中。
+    // 2.1 subscribeNext内部会调用siganl的didSubscribe
+    // 3.siganl的didSubscribe中调用[subscriber sendNext:@1];
+    // 3.1 sendNext底层其实就是执行subscriber的nextBlock
+    
+    // 1.创建信号
+    RACSignal *siganl = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        
+        // block调用时刻：每当有订阅者订阅信号，就会调用block。
+        
+        // 2.发送信号
+        [subscriber sendNext:@66];
+        
+        // 如果不在发送数据，最好发送信号完成，内部会自动调用[RACDisposable disposable]取消订阅信号。
+        [subscriber sendCompleted];
+        
+        return [RACDisposable disposableWithBlock:^{
+            
+            // block调用时刻：当信号发送完成或者发送错误，就会自动执行这个block,取消订阅信号。
+            
+            // 执行完Block后，当前信号就不在被订阅了。
+            
+            NSLog(@"信号被销毁");
+            
+        }];
+    }];
+    
+    // 3.订阅信号,才会激活信号.
+    [siganl subscribeNext:^(id x) {
+        // block调用时刻：每当有信号发出数据，就会调用block.
+        NSLog(@"接收到数据:%@",x);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
