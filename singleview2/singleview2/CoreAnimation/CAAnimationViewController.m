f87440ed67fe49613ae0ec8040cdcda7128f95ce
feiatc <qhk83@qq.com>
2019-02-19 23:26:27 +0800

    test get super class and alloc init

--- singleview2/singleview2/CoreAnimation/CAAnimationViewController.m
+++ singleview2/singleview2/CoreAnimation/CAAnimationViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CAAnimationViewController.h"
+#import "ArtistData.h"
 #import <objc/runtime.h>
 
 @interface CAAnimationViewController ()
@@ -45,19 +46,44 @@
 //    [self performSelector:@selector(rotation) withObject:nil afterDelay:1.0];
     [self performSelector:@selector(changeToNextImage) withObject:bezierPath afterDelay:1.0];
 
-    Class class = [self class];//类对象
+    //https://blog.csdn.net/Hello_Hwc/article/details/49687543
+
+    Class class = [Artist class];//类对象
     Class metaClass = object_getClass(class);//类元对象
     Class metaOfMetaClass = object_getClass(metaClass);//NSObject类元对象
     Class rootMataClass = object_getClass(metaOfMetaClass);//NSObject类元对象的类元对象
 
     NSLog(@"CustomObject类对象是:%p %@", class, NSStringFromClass(class));
     NSLog(@"CustomObject类元对象是:%p %@", metaClass, NSStringFromClass(metaClass));
+    NSLog(@"CustomObject类元对象的父对象是:%p", class_getSuperclass(metaClass));
     NSLog(@"metaClass类元对象:%p %@", metaOfMetaClass, NSStringFromClass(metaOfMetaClass));
+    NSLog(@"metaClass类元对象的父对象是:%p", class_getSuperclass(metaOfMetaClass));
     NSLog(@"metaOfMetaClass的类元对象的是:%p %@", rootMataClass, NSStringFromClass(rootMataClass));
 
     Class objClass = [NSObject class];
     NSLog(@"NSObject类元对象%p %@", object_getClass(objClass), NSStringFromClass(objClass));
 
+    NSLog(@" ---------");
+
+    Class clazz = [Artist class];//类对象
+    Class superClass = class_getSuperclass(clazz);//基类对象NSObject
+    Class superOfNSObject = class_getSuperclass(superClass);//NSObject类元对象
+
+    NSLog(@"CustomObject类对象是:%p %@", clazz, NSStringFromClass(clazz));
+    NSLog(@"CustomObject类superClass是:%p %@", superClass, NSStringFromClass(superClass));
+    NSLog(@"CustomObject类superClass的类元对象是:%p", object_getClass(superClass));
+    NSLog(@"NSObject的superClass是:%p %@", superOfNSObject, NSStringFromClass(superOfNSObject));
+
+    NSLog(@" ---------");
+
+    id a = [NSMutableArray alloc];
+    id b = [a init];
+    NSLog(@"NSMutableArray %p %p", a, b); //NSMutableArray 0x283540050 0x28391e8e0
+
+    a = [NSArray alloc];
+    b = [a init];
+    NSLog(@"NSArray %p %p", a, b); //NSArray 0x283540060 0x283540080
+    
 }
 
 - (void)rotation {
