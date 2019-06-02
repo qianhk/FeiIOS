ddc2da54416d8c85bd9190ea9a83b490c68da592
qianhk <qhk83@qq.com>
2019-06-02 13:41:42 +0800

    add new test page

--- singleview2/singleview2/EntryTabViewController.m
+++ singleview2/singleview2/EntryTabViewController.m
@@ -49,6 +49,7 @@
             @{@"Name": @"EmitterTestViewController", @"Vc": @"EmitterTestViewController"},
             @{@"Name": @"AnchorPointViewController", @"Vc": @"AnchorPointViewController", @"Nib": @"AnchorPointViewController"},
             @{@"Name": @"CAAnimationViewController", @"Vc": @"CAAnimationViewController", @"Nib": @"CAAnimationViewController"},
+            @{@"Name": @"TestManyLayerViewController", @"Vc": @"TestManyLayerViewController", @"Nib": @"TestManyLayerViewController"},
             @{@"Name": @"LinearLayoutViewController", @"Vc": @"TestLinearLayoutViewController"},
             @{@"Name": @"AnimationTestViewController", @"Vc": @"AnimationTestViewController", @"Nib": @"AnimationTestViewController"},
             @{@"Name": @"SubscribeViewController", @"Vc": @"SubscribeViewController"},
